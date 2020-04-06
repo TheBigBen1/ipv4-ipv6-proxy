66b9e586a8e53beaace4eccdb3e23ad674bb3b7d
BigBen <42361307+TheBigBen1@users.noreply.github.com>
2020-04-06 02:46:47 +0100

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-#bash <(curl -s "https://raw.githubusercontent.com/dukaev/ipv6_proxy/master/scripts/install.sh")
-# sudo passwd ubuntu
-# test
 random() {
 	tr </dev/urandom -dc A-Za-z0-9 | head -c5
 	echo
@@ -9,12 +6,11 @@ random() {
 
 array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
 gen64() {
-    ip64() {
-    	echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
-    }
+	ip64() {
+		echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+	}
 	echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
 }
-
 install_3proxy() {
     echo "installing 3proxy"
     URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
@@ -22,15 +18,15 @@ install_3proxy() {
     cd 3proxy-3proxy-0.8.6
     make -f Makefile.Linux
     mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
-    sudo cp src/3proxy /usr/local/etc/3proxy/bin/
+    cp src/3proxy /usr/local/etc/3proxy/bin/
     cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
-    sudo chmod +x /etc/init.d/3proxy
-    yes test | update-rc.d 3proxy start 20 3 4 5 $test
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
     cd $WORKDIR
 }
 
 gen_3proxy() {
-    sudo cat <<EOF
+    cat <<EOF
 daemon
 maxconn 1000
 nscache 65536
@@ -67,7 +63,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+        echo "usrplsfknwork/passbigben/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
@@ -82,20 +78,15 @@ gen_ifconfig() {
 $(awk -F "/" '{print "ifconfig eth0 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
 }
-
-
-
-
 echo "installing apps"
-#yum -y install gcc net-tools bsdtar zip >/dev/null
-sudo apt-get install gcc net-tools bsdtar zip >/dev/null
+yum -y install gcc net-tools bsdtar zip >/dev/null
 
 install_3proxy
 
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-sudo mkdir $WORKDIR && cd $_
+mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s ifconfig.co)
 IP6=$(curl -6 -s ifconfig.co | cut -f1-4 -d':')
@@ -108,23 +99,30 @@ read COUNT
 FIRST_PORT=10000
 LAST_PORT=$(($FIRST_PORT + $COUNT))
 
-sudo chmod a+rwx $WORKDIR/
+echo "creating data file"
 gen_data >$WORKDIR/data.txt
+echo "creating iptables"
 gen_iptables >$WORKDIR/boot_iptables.sh
+echo "creating ipconfig"
 gen_ifconfig >$WORKDIR/boot_ifconfig.sh
-sudo chmod +x boot_*.sh /etc/rc.local
+chmod +x boot_*.sh /etc/rc.local
 
-gen_3proxy > sudo /usr/local/etc/3proxy/3proxy.cfg
 
-cat >> sudo /etc/rc.local <<EOF
-sudo bash /home/proxy-installer/boot_iptables.sh
-bash /home/proxy-installer/boot_ifconfig.sh
+echo "gen3proxy shit"
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
+echo "iptables"
+bash ${WORKDIR}/boot_iptables.sh
+echo "ipconfig"
+bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 2048
-sudo service 3proxy start
+service 3proxy start
 EOF
 
 bash /etc/rc.local
-
+echo "creating proxy file"
 gen_proxy_file_for_user
 
+echo "uploading file"
 upload_proxy
